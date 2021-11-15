dc3b15a89b12f110ff6d60f0123ff7ec3c8f96f4
Fyefee <jeffychawin@gmail.com>
2021-11-15 14:10:43 +0000

    Submit Assignment 1 Deploy Service with Helm Chart (Fix DB Bug for all env)

--- k8s/deploy-helm-bookinfo-uat-ratings.sh
+++ k8s/deploy-helm-bookinfo-uat-ratings.sh
@@ -1,3 +1,8 @@
+# curl http://itkmitl.bookinfo.uat.opsta.net/student19/ratings/health
+# curl http://itkmitl.bookinfo.uat.opsta.net/student19/ratings/ratings/1
+helm install mongodb bitnami/mongodb --set persistence.enabled=false
+export MONGODB_ROOT_PASSWORD=$(kubectl get secret mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
+helm uninstall mongodb
 kubectl create secret generic registry-bookinfo --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
 kubectl create secret generic bookinfo-uat-ratings-mongodb-secret --from-literal=mongodb-password=CHANGEME --from-literal=mongodb-root-password=CHANGEME
 kubectl create configmap bookinfo-uat-ratings-mongodb-initdb --from-file=databases/ratings_data.json --from-file=databases/script.sh
